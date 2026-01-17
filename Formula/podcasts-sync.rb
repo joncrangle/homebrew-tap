0e2b1b5c5ff7c287b7efb1d5af616fea498e5c8f
joncrangle <94425204+joncrangle@users.noreply.github.com>
2026-01-17 12:44:32 -0500

    Update podcasts-sync URLs to version 0.3.1

--- Formula/podcasts-sync.rb
+++ Formula/podcasts-sync.rb
@@ -5,10 +5,10 @@ class PodcastsSync < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joncrangle/podcasts-sync/releases/download/v0.2.1/podcasts-sync_Darwin_arm64.tar.gz"
+      url "https://github.com/joncrangle/podcasts-sync/releases/download/v0.3.1/podcasts-sync_Darwin_arm64.tar.gz"
       sha256 "062f7ccd4208c36fad8de65f6c59fb913ef5a079226deef401ea053ce34398b6"
     else
-      url "https://github.com/joncrangle/podcasts-sync/releases/download/v0.2.1/podcasts-sync_Darwin_x86_64.tar.gz"
+      url "https://github.com/joncrangle/podcasts-sync/releases/download/v0.3.1/podcasts-sync_Darwin_x86_64.tar.gz"
       sha256 "11614a4f42c8f5310bd9f71c9c5a5579a901100c4646db946aa83152660234ed"
     end
   end
