c1713ac5ccdfbbaeaf176647d6683778d592a314
joncrangle <94425204+joncrangle@users.noreply.github.com>
2026-01-17 12:48:24 -0500

    Fix URLs for sketchybar-system-stats downloads

--- Formula/sketchybar-system-stats.rb
+++ Formula/sketchybar-system-stats.rb
@@ -5,10 +5,10 @@ class SketchybarSystemStats < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/joncrangle/sketchybar-system-stats/releases/download/0.8.1/stats_provider-refs/tags/0.8.1-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/joncrangle/sketchybar-system-stats/releases/download/0.8.1/stats_provider-0.8.1-aarch64-apple-darwin.tar.gz"
       sha256 "37f0e0a66c0339497ee3d3b51db0fd96e9fcc90f5bdf1b53d6ec99606e6caf97"
     else
-      url "https://github.com/joncrangle/sketchybar-system-stats/releases/download/0.8.1/stats_provider-refs/tags/0.8.1-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/joncrangle/sketchybar-system-stats/releases/download/0.8.1/stats_provider-0.8.1-x86_64-apple-darwin.tar.gz"
       sha256 "adaff51648ab0d74ea82106ad7b8d3b5776831fd96663f3b8c522cfac659dfa2"
     end
   end
